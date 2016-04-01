de67e8517c51e424b92b8a09cc40e503a0d8973d
Joseph <emkev@users.noreply.github.com>
2016-04-02 01:42:30 +0800

    Create plotcos.c
    plot a cos curve

---
+++ plotcos.c
@@ -0,0 +1,30 @@
+
+/* 20151202 23:32 start . Plot cos-curve with pringf() .  */
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+  double y ;
+  int x , m ;
+  
+  for(y = 1 ; y >= -1 ; y -= 0.1)
+  {
+    m = acos(y) * 10 ;
+    for(x = 0 ; x < m ; x++)
+      printf(" ");
+
+    printf("*");
+
+    for(x = m + 1 ; x < 62 - m ; x++)
+      printf(" ");
+
+    printf("*\n");
+  }
+  
+  return 0 ;
+  //exit(0);
+
+}
